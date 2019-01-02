2e4ba32f0519f3a2d0bb3609fb21737daf8088f9
liarum <lar404@naver.com>
2019-01-02 17:49:22 +0900

    ss3 | python intro | ~190102 | r2

--- 0.startcamp/day6/make_GCD_and_LCM.py
+++ 0.startcamp/day6/make_GCD_and_LCM.py
@@ -12,11 +12,11 @@ for i in range(1, num2+1, 1):
 for gcd in divisor_num1:
     if gcd in divisor_num2:
         greatest_common_divisor = gcd
-print(greatest_common_divisor)
+print('최대 공약수는 ', greatest_common_divisor)
     
 # 10과 15를 소인수 분해한다? 1부터 시작해서 n까지 나머지가 0이 되게 나누는 수를 구한다
 # l_n1을 돌면서 ㅣ_n2와 일치하는 수 중 제일 큰 수를 구한다
 # 두 수를 곱하고 최대공약수로 나눈다
 
 least_common_multiple = num1*num2 // greatest_common_divisor
-print(least_common_multiple)
+print('최소 공배수는 ', least_common_multiple)
